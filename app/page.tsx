b317059f0c531f5382666b2b7459bb21e93754bf
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-03-04 19:00:39 +0300

    Update API endpoints to use relative paths

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
   const fetchAllTraits = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/countAllTraits', {
+      const response = await fetch('/api/countAllTraits', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -44,7 +44,7 @@ export default function Home() {
 
   const fetchNFTs = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/getNFTsByTraits', {
+      const response = await fetch('/api/getNFTsByTraits', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
