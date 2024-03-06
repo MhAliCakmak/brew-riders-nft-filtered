3a046f6c0913361cb9ed575dd0825f65dbac7376
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-03-06 14:44:15 +0300

    Add Access-Control-Allow-Origin header to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
         // !! WARN !!
         ignoreBuildErrors: true,
       },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
 };
 
 export default nextConfig;
